db52914c727936aa8cc83a88e4dcdbf984f6363d
Miki <majormiki96@hotmail.com>
2023-09-26 16:33:18 +0200

    uploaded-screen

--- angular/iReportUI/src/app/app.component.ts
+++ angular/iReportUI/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'iReportUI';
+  report :string | undefined;
+  onSubmit(): void{
+    this.report = "UPLOADED"
+  }
+
 }
